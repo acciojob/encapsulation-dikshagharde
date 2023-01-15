af236172a3835dbf20ac152962b517eb81ec2720
dikshagharde <dikshagharde23@gmail.com>
2023-01-15 17:53:14 +0530

    Encapsulation Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+    obj.setName("Diksha");
+    System.out.println("Error: you might seen some error, please not it down in commnets");
+    obj.getName();
+  }
 }
